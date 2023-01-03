a40ad9986521d2f97231cfa3332fd257cbedcd1d
Deepak Subedi <deepak.subedi311@gmail.com>
2023-01-04 01:46:51 +0900

    updated

---
+++ scripts.groovy
@@ -0,0 +1,17 @@
+def buildJar() {
+    echo "Building the application..."
+    sh "mvn package"
+}
+
+def buildImage() {
+    echo "Building the docker image..."
+    withCredentials([usernamePassword(credentialsId: "docker-hub-credentials", usernameVariable: "USERNAME", passwordVariable: "PASSWORD")]) {
+        sh "docker build -t $IMAGE_NAME:$IMAGE_VERSION ."
+        sh "echo $PASSWORD | docker login -u $USERNAME --password-stdin"
+        sh "docker push $IMAGE_NAME:$IMAGE_VERSION"
+    }
+}
+
+def deployApp() {
+    echo "Deploying image..."
+}
