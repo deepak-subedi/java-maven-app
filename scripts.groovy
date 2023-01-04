09668552ed40fee75d3b049ad5a410cba1c856be
Deepak Subedi <deepak.subedi311@gmail.com>
2023-01-05 01:46:55 +0900

    added version increment steps in jenkins pipeline

--- scripts.groovy
+++ scripts.groovy
@@ -5,7 +5,7 @@ def incrementVersion() {
         versions:commit'
     def matcher = readFile('pom.xml') =~ '<version>(.+)</version>'
     def version = matcher[0][1]
-    env.IMAGE_VERSION = "$version=$BUILD_NUMBER"
+    env.IMAGE_VERSION = "$version-$BUILD_NUMBER"
 }
 
 def buildJar() {
