d59f303469bcb71cf977dfedd22741da99d07266
Deepak Subedi <deepak.subedi311@gmail.com>
2023-01-05 01:44:43 +0900

    added version increment steps in jenkins pipeline

--- scripts.groovy
+++ scripts.groovy
@@ -1,6 +1,6 @@
 def incrementVersion() {
     echo "Increasing the app version..."
-    sh 'mvn build-helper:parse-version version:set \
+    sh 'mvn build-helper:parse-version versions:set \
         -DnewVersion=\\\${parsedVersion.majorVersion}.\\\${parsedVersion.minorVersion}.\\\${parsedVersion.nextIncrementalVersion} \
         versions:commit'
     def matcher = readFile('pom.xml') =~ '<version>(.+)</version>'
