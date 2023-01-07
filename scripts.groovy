6dc32efb0152ee7bb52b6873fb34f109469542d9
Deepak Subedi <deepak.subedi311@gmail.com>
2023-01-07 20:57:50 +0900

    added updated groovy file

--- scripts.groovy
+++ scripts.groovy
@@ -24,7 +24,8 @@ def buildImage() {
 
 def commitChanges() {
     echo "Update Pom.xml file to git repository..."
-    withCredentials([usernamePassword(credentialsId: "github-credentials", usernameVariable: "USERNAME", passwordVariable: "PASSWORD")]) {        
+    withCredentials([usernamePassword(credentialsId: "github-credentials", usernameVariable: "USERNAME", passwordVariable: "PASSWORD")]) {       
+        sh "git config --global --unset http.proxy" 
         sh 'git config --global user.email "jenkins@gmail.com"'
         sh 'git config --global user.name "jenkins"'
 
@@ -32,7 +33,7 @@ def commitChanges() {
         sh "git branch"
         sh "git config --list"
 
-        sh "git remote set-url origin https://$USERNAME:$PASSWORD@github.com/deepak-subedi/java-maven-app.git"
+        sh "git remote set-url origin https://${USERNAME}:${PASSWORD}@github.com/deepak-subedi/java-maven-app.git"
         sh "git add ."
         sh 'git commit -m "ci: version bump"'
         sh "git push origin HEAD:main"
