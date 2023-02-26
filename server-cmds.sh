2a881388f47c97206b735f514ea6e94b6a84fd25
Deepak Subedi <deepak.subedi311@gmail.com>
2023-02-26 22:28:27 +0900

    updated docker compose command from docker compose to docker-compose

--- server-cmds.sh
+++ server-cmds.sh
@@ -5,8 +5,8 @@ export IMAGE_VERSION=$2
 export DOCKER_USER=$3
 export DOCKER_PWD=$4
 
-# docker compose -f docker-compose.yaml down
-# docker rmi $(docker images -q)
+docker-compose -f docker-compose.yaml down
+docker rmi $(docker images -q)
 
 echo $DOCKER_PWD | docker login -u $DOCKER_USER --password-stdin
 docker-compose -f docker-compose.yaml up --detach
